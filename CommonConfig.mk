ead0ba5f07e3d55f3a05a8625577e0b493b2a40e
ix5 <ix5@users.noreply.github.com>
2019-02-14 16:22:32 +0100

    Disable SELinux entirely

--- CommonConfig.mk
+++ CommonConfig.mk
@@ -30,6 +30,7 @@ TARGET_NO_KERNEL := false
 # common cmdline parameters
 ifneq ($(BOARD_USE_ENFORCING_SELINUX),true)
 BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
+BOARD_KERNEL_CMDLINE += selinux=0
 endif
 #BOARD_KERNEL_CMDLINE += console=ttyMSM0,115200,n8 androidboot.console=ttyMSM0
 BOARD_KERNEL_CMDLINE += msm_rtb.filter=0x3F ehci-hcd.park=3
